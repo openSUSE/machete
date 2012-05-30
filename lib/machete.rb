01388bc620d95cf161b3d6ff116caa09b4198329
Piotr Niełacny <piotr.nielacny@gmail.com>
2012-05-30 10:47:10 +0200

    Add Machete DSL. Extend core classes, add .to_m method.
    Add specs for Machete DSL
    Refactoring
    Add specs for Fixnum, NilClass and Array
    Update SUSE code conventions
    Don not use boolean arguments
    Remove should from specs
    Check in specs whole hashes instead of sub-hashes

--- lib/machete.rb
+++ lib/machete.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/machete/matchers")
 require File.expand_path(File.dirname(__FILE__) + "/machete/parser")
 require File.expand_path(File.dirname(__FILE__) + "/machete/version")
+require_relative "machete/dsl"
 
 module Machete
   # Matches a Rubinius AST node against a pattern.
---
+++ lib/machete/core_ext/nil.rb
@@ -0,0 +1,5 @@
+class NilClass
+  def to_m
+    to_s
+  end
+end
