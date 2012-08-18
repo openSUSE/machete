93269bc2d791a0ba03530d36ee892edde99f4e32
David Majda <dmajda@suse.cz>
2012-08-18 14:57:11 +0200

    Update & improve Machete#{matches?,find} API docs

--- lib/machete.rb
+++ lib/machete.rb
@@ -7,17 +7,36 @@ module Machete
     # Matches a Rubinius AST node against a pattern.
     #
     # @param [Rubinius::AST::Node] node node to match
-    # @param [String] pattern pattern to match the node against (see {file:README.md} for syntax description)
+    # @param [String, Machete::Matchers::Matcher] pattern pattern to match the
+    #   node against, either as a string (see {file:README.md} for syntax
+    #   description) or in compiled form
     #
-    # @example Succesfull match
-    #   Machete.matches?('foo.bar'.to_ast, 'Send<receiver = Send<receiver = Self, name = :foo>, name = :bar>')
+    # @example Test using a string pattern
+    #   Machete.matches?('foo.bar'.to_ast, 'Send<name = :bar>')
     #   # => true
     #
-    # @example Failed match
-    #   Machete.matches?('42'.to_ast, 'Send<receiver = Send<receiver = Self, name = :foo>, name = :bar>')
+    #   Machete.matches?('42'.to_ast, 'Send<name = :bar>')
     #   # => false
     #
-    # @return [Boolean] +true+ if the node matches the pattern, +false+ otherwise
+    # @example Test using a compiled pattern
+    #   Machete.matches?(
+    #     'foo.bar'.to_ast,
+    #     Machete::Matchers::NodeMatcher.new("Send",
+    #       :name => Machete::Matchers::LiteralMatcher.new(:bar)
+    #     )
+    #   )
+    #   # => true
+    #
+    #   Machete.matches?(
+    #     '42'.to_ast,
+    #     Machete::Matchers::NodeMatcher.new("Send",
+    #       :name => Machete::Matchers::LiteralMatcher.new(:bar)
+    #     )
+    #   )
+    #   # => false
+    #
+    # @return [Boolean] +true+ if the node matches the pattern, +false+
+    #   otherwise
     #
     # @raise [Matchete::Parser::SyntaxError] if the pattern is invalid
     def matches?(node, pattern)
@@ -27,18 +46,23 @@ module Machete
     # Finds all nodes in a Rubinius AST matching a pattern.
     #
     # @param [Rubinius::AST::Node] ast tree to search
-    # @param [String, Machete::Matchers] pattern pattern to match the nodes against (see {file:README.md} for syntax description)
+    # @param [String, Machete::Matchers::Matcher] pattern pattern to match the
+    #   nodes against, either as a string (see {file:README.md} for syntax
+    #   description) or in compiled form
     #
-    # @example
+    # @example Search using a string pattern
     #   Machete.find('42 + 43 + 44'.to_ast, 'FixnumLiteral')
     #   # => [
     #   #      #<Rubinius::AST::FixnumLiteral:0x10b0 @value=44 @line=1>,
     #   #      #<Rubinius::AST::FixnumLiteral:0x10b8 @value=43 @line=1>,
     #   #      #<Rubinius::AST::FixnumLiteral:0x10c0 @value=42 @line=1>
     #   #    ]
-    # @example
-    #   compiled_pattern = Machete::Parser.new.parse('FixnumLiteral')
-    #   Machete.find('42 + 43 + 44'.to_ast, compiled_pattern)
+    #
+    # @example Search using a compiled pattern
+    #   Machete.find(
+    #     '42 + 43 + 44'.to_ast,
+    #     Machete::Matchers::NodeMatcher.new("FixnumLiteral")
+    #   )
     #   # => [
     #   #      #<Rubinius::AST::FixnumLiteral:0x10b0 @value=44 @line=1>,
     #   #      #<Rubinius::AST::FixnumLiteral:0x10b8 @value=43 @line=1>,
